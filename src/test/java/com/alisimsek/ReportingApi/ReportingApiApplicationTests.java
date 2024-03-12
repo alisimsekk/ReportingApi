c6111b24ad63f2acfda2d145b8aa3e7bf9a53bc4
alisimsekk <ali.simsek035@gmail.com>
2024-03-13 00:15:16 +0300

    Initial commit

---
+++ src/test/java/com/alisimsek/ReportingApi/ReportingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alisimsek.ReportingApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
