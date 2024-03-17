6c269f3a58bcc36861c756f4a029337224147496
alisimsekk <ali.simsek035@gmail.com>
2024-03-17 07:41:07 +0300

    Updated CustomerInfoDto

--- src/main/java/com/alisimsek/ReportingApi/dto/response/client/CustomerInfoDto.java
+++ src/main/java/com/alisimsek/ReportingApi/dto/response/client/CustomerInfoDto.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
@@ -12,17 +12,52 @@ import java.util.Date;
 @NoArgsConstructor
 public class CustomerInfoDto {
 
-    private String billingFirstName;
-    private String billingLastName;
-    private String email;
-    private String billingCompany;
-    private String billingCity;
+    private Long id;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private Date created_at;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updated_at;
-    private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime deletedAt;
+    private String number;
+    private String expiryMonth;
+    private String expiryYear;
+    private String startMonth;
+    private String startYear;
+    private String issueNumber;
+    private String email;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime birthday;
+    private String gender;
+
+    private String billingTitle;
+    private String billingFirstName;
+    private String billingLastName;
+    private String billingCompany;
     private String billingAddress1;
+    private String billingAddress2;
+    private String billingCity;
+    private String billingPostcode;
+    private String billingState;
+    private String billingCountry;
+    private String billingPhone;
+    private String billingFax;
+
+    private String shippingTitle;
+    private String shippingFirstName;
+    private String shippingLastName;
+    private String shippingCompany;
+    private String shippingAddress1;
+    private String shippingAddress2;
+    private String shippingCity;
+    private String shippingPostcode;
+    private String shippingState;
+    private String shippingCountry;
+    private String shippingPhone;
+    private String shippingFax;
+    private String token;
 }
